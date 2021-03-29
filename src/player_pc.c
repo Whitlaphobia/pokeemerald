464d744ec3b124986b1cac6451a162471d7703ae
Whitlaphobia <nic.whitla@outlook.com>
2021-03-29 10:39:39 -0400

    TM Infinite Use
    Made tms infinite use, fixed shop and game corner interactions. Put it in the pc to test it

--- src/player_pc.c
+++ src/player_pc.c
@@ -170,6 +170,7 @@ static const struct MenuAction gPCText_ItemPCOptionsText[] =
 static const struct ItemSlot gNewGamePCItems[] =
 {
     { ITEM_POTION, 1 },
+    { ITEM_TM13, 1 },
     { ITEM_NONE, 0 }
 };
 
