8018067c8bd27a331f39599cb4c79e196866f8c3
Whitlaphobia <nic.whitla@outlook.com>
2021-04-22 16:41:47 -0400

    Map Majiging

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1072,3 +1072,5 @@ Common_EventScript_LegendaryFlewAway:: @ 8273776
 	.include "data/maps/LittlerootTown_WeatherInstitute_3F_B/scripts.inc"
 
 	.include "data/maps/LittlerootTown_WeatherInstitute_3F_C/scripts.inc"
+
+	.include "data/maps/LittlerootTown_UnderseaTours_1F/scripts.inc"
